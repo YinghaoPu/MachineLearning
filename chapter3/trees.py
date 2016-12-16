bb79a8fa742c0137f4351414a734cbf69f3e3175
HugoPu <puyh@inspurworld.com>
2016-12-16 18:16:50 +0800

    add code

--- chapter3/trees.py
+++ chapter3/trees.py
@@ -4,7 +4,7 @@ def calc_shannon_ent(data_set):
     num_entries = len(data_set)
     label_counts = {}
     for feat_vec in data_set:
-        current_label = list(feat_vec)[-1]
+        current_label = feat_vec[-1]
         if current_label not in label_counts.keys():
             label_counts[current_label] = 0
         label_counts[current_label] += 1
@@ -15,10 +15,38 @@ def calc_shannon_ent(data_set):
     return shannon_ent
 
 def create_data_set():
-    data_set = [{1, 1, 'yes'},
-                {1, 1, 'yes'},
-                {1, 0, 'no'},
-                {0, 1, 'no'},
-                {0, 1, 'no'}]
+    data_set = [[1, 1, 'yes'],
+                [1, 1, 'yes'],
+                [1, 0, 'no'],
+                [0, 1, 'no'],
+                [0, 1, 'no']]
     labels = ['no surfacing', 'flippers']
     return data_set, labels
+
+def split_data_set(data_set, axis, value):
+    ret_data_set = []
+    for feat_vec in data_set:
+        if feat_vec[axis] == value:
+            reduced_feat_vec = feat_vec[:axis]
+            reduced_feat_vec.extend(feat_vec[axis+1:])
+            ret_data_set.append(reduced_feat_vec)
+    return reduced_feat_vec
+
+def choose_best_feature_to_split(data_set):
+    num_features = len(data_set[0]) - 1
+    base_entropy = calc_shannon_ent(data_set)
+    best_info_gain = 0.0
+    best_feature = -1
+    for i in range(num_features):
+        feature_list = [example[i] for example in data_set]
+        unique_value = set(feature_list)
+        new_entropy = 0.0
+        for value in unique_value:
+            sub_data_set = split_data_set(data_set, i, value)
+            prob = len(sub_data_set) / float(len(data_set))
+            new_entropy += prob * calc_shannon_ent(sub_data_set)
+        info_gain = base_entropy - new_entropy
+        if info_gain > best_info_gain:
+            best_feature = info_gain
+            best_feature = i
+    return best_feature
