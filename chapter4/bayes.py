af4e601197ba7a6606a4467d34562a4be1a0fbb5
HugoPu <puyh@inspurworld.com>
2017-01-04 11:17:00 +0800

    no comments

---
+++ chapter4/bayes.py
@@ -0,0 +1,25 @@
+# create vacabulary list
+def load_dataset():
+    posting_list = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
+                    ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+                    ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+                    ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+                    ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him']
+                    ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+    class_vec = [0, 1, 0, 1, 0, 1]
+    return posting_list, class_vec
+# create vocabulary set
+def create_vocab_list(dataset):
+    vocab_set = set([])
+    for document in dataset:
+        vocab_set = vocab_set | set(document)
+    return list(vocab_set)
+
+# return mapping list from input_set on vocabulary set
+def words_to_vec_set(vocab_list, input_set):
+    return_vec = [0]*len(vocab_list)
+    for word in input_set:
+        if word in vocab_list:
+            return_vec[vocab_list.index(word)] = 1
+        else: print "the word: %s is not in my vocabulary!" % word
+    return return_vec
--- run.py
+++
@@ -1,5 +0,0 @@
-from chapter3 import trees
-
-my_data_set, labels = trees.create_data_set()
-result = trees.calc_shannon_ent(my_data_set)
-print result
